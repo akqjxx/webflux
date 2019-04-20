1555b52efe1cf750d572934c66f5453f18768bff
akqjxx <liuer2595919@163.com>
2019-04-20 22:58:49 +0800

    1

---
+++ src/test/java/cn/com/et/route/CityRouter.java
@@ -0,0 +1,25 @@
+package cn.com.et.route;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import cn.com.et.handle.CityHandler;
+
+@Configuration
+public class CityRouter {
+
+
+    @Bean
+    public RouterFunction<ServerResponse> routeCity(CityHandler cityHandler) {
+        return RouterFunctions
+                .route(RequestPredicates.GET("/hello")
+                                .and(RequestPredicates.accept(MediaType.TEXT_PLAIN)),
+                        cityHandler::helloCity);
+    }
+
+}
